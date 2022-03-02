89e752161f3351fa26a806da661463fa00c4b08c
J0EK3R <J0EK3R@users.noreply.github.com>
2022-03-02 05:30:59 +0100

    renamed controls.txt to controls.txt_fhem_fordpass.txt

--- prepare_update.sh
+++ prepare_update.sh
@@ -7,10 +7,10 @@ do
   FILESIZE=$(stat -c%s "$FILE")
 	FILE=$(echo "$FILE"  | cut -c 3-)
 	#printf "DEL %s\n" "$FILE"  >> controls.txt
-	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls.txt
+	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_fhem_fordpass.txt
 done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" -o -name "*.txt" \) -print0 | sort -z -g)
 
 # CHANGED file
-echo "FHEM Grohe Ondus last changes:" > CHANGED
+echo "FHEM fordpass last changes:" > CHANGED
 echo $(date +"%Y-%m-%d") >> CHANGED
 echo " - $(git log -1 --pretty=%B)" >> CHANGED
